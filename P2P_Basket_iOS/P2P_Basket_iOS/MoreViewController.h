b97dc2951d14debb7b845594d8d9a1d90ea9d0c3
inkJake <inkjake@gmail.com>
2014-11-15 23:42:12 +0800

    文档添加

---
+++ P2P_Basket_iOS/P2P_Basket_iOS/MoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreViewController.h
+//  P2P_Basket_iOS
+//
+//  Created by inkJake on 11/15/14.
+//  Copyright (c) 2014 inkJake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreViewController : UIViewController
+
+@end
---
+++ P2P_Basket_iOS/P2P_Basket_iOS/MoreViewController.m
@@ -0,0 +1,37 @@
+//
+//  MoreViewController.m
+//  P2P_Basket_iOS
+//
+//  Created by inkJake on 11/15/14.
+//  Copyright (c) 2014 inkJake. All rights reserved.
+//
+
+#import "MoreViewController.h"
+
+@interface MoreViewController ()
+
+@end
+
+@implementation MoreViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
