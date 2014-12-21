453ee3fed47bd125324ab90b018b9eb0694c4e0d
Crowley <castiel.zyf@gmail.com>
2014-12-21 23:29:10 +0800

    完成了主页的即将到期与已经到期，完成了流水的排序，完成了新的投资，能创建新的投资记录

--- P2P_Basket_Android/src/com/crowley/p2pnote/IndexFragment.java
+++ P2P_Basket_Android/src/com/crowley/p2pnote/IndexFragment.java
@@ -5,9 +5,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.crowley.p2pnote.db.DBOpenHelper;
+import com.crowley.p2pnote.functions.ReturnList;
+import com.crowley.p2pnote.ui.listAdapter;
+
 import android.R.integer;
 import android.app.Fragment;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -20,13 +28,16 @@ import android.widget.TextView;
 public class IndexFragment extends Fragment implements OnClickListener{	
 
 	private View view;
-	
+	private ReturnList returnList;
 	private ListView listView;
-	private SimpleAdapter list_adapter;
+	private listAdapter list_adapter;
 	private List<Map<String, Object>> dataList;
 	
 	private LinearLayout tab_button01;
 	private LinearLayout tab_button02;
+	
+	private TextView tab_button01_number;
+	private TextView tab_button02_number;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,15 +46,19 @@ public class IndexFragment extends Fragment implements OnClickListener{
 		View view = inflater.inflate(R.layout.index_fragment, container, false);
 		//TextView text = (TextView) view.findViewById(R.id.test);
 		
-
+		returnList = new ReturnList();
         dataList=new ArrayList<Map<String,Object>>();
         listView=(ListView) view.findViewById(R.id.list_view);
         getData(1);
-        list_adapter=new SimpleAdapter(this.getActivity(), dataList, R.layout.index_listview_item, new String[]{"time","item_icon","item_name","item_money","item_profit"}, new int[]{R.id.time,R.id.item_icon,R.id.item_name,R.id.item_money,R.id.item_profit});
+        list_adapter=new listAdapter(this.getActivity(), dataList, R.layout.index_listview_item, new String[]{"time","item_icon","item_name","item_money","item_profit"}, new int[]{R.id.time,R.id.item_icon,R.id.item_name,R.id.item_money,R.id.item_profit});
         listView.setAdapter(list_adapter);
         
         tab_button01 = (LinearLayout) view.findViewById(R.id.tab_button01);
         tab_button02 = (LinearLayout) view.findViewById(R.id.tab_button02);
+        tab_button01_number = (TextView) view.findViewById(R.id.tab_button01_number);
+        tab_button02_number = (TextView) view.findViewById(R.id.tab_button02_number);
+        tab_button01_number.setText(String.valueOf(returnList.indexCount(this.getActivity(), 0)));
+        tab_button02_number.setText(String.valueOf(returnList.indexCount(this.getActivity(), 1)));
 		
         tab_button01.setOnClickListener(this);
         tab_button02.setOnClickListener(this);
@@ -52,26 +67,14 @@ public class IndexFragment extends Fragment implements OnClickListener{
 	}
 
 	//private List<Map<String, Object>> getData(int index){
-	private void getData(int index){
+	//0代表已经过期，1代表即将过期
+	private void getData(int type){
 		dataList.clear();
-    	for(int i=0;i<20;i++){
-    		Map<String, Object> map=new HashMap<String, Object>();
-    		if(index==0){
-    			map.put("time", "2014-10-1"+i);
-        		map.put("item_icon", R.drawable.company_icon02);
-        		map.put("item_name", "陆金所-富赢人生");
-        		map.put("item_money", "12,000");
-        		map.put("item_profit", "12%");
-    		}else{    			
-        		map.put("time", "2014-08-12");
-        		map.put("item_icon", R.drawable.company_icon01);
-        		map.put("item_name", "人人贷-优选计划");
-        		map.put("item_money", "11,000");
-        		map.put("item_profit", "8%");
-    		}    		
-    		dataList.add(map);    		
-    	}
-    	//return dataList;
+		List<Map<String, Object>> temp=new ArrayList<Map<String,Object>>();
+		temp=returnList.indexList(this.getActivity(), type);
+		for(int i=0;i<temp.size();i++){
+			dataList.add(temp.get(i));
+		}
     }
 
 	@Override
@@ -84,20 +87,12 @@ public class IndexFragment extends Fragment implements OnClickListener{
             tab_button02.setBackgroundColor(getResources().getColor(R.color.tab_bg));
             getData(0);
             list_adapter.notifyDataSetChanged();
-            for(int i = 0;i<listView.getChildCount();i++){
-            	TextView textView= (TextView) listView.getChildAt(i).findViewById(R.id.item_name);
-            	textView.setTextColor(getResources().getColor(R.color.company02));
-            }            
             break;  
         case R.id.tab_button02:  
         	tab_button01.setBackgroundColor(getResources().getColor(R.color.tab_bg));  
             tab_button02.setBackgroundColor(getResources().getColor(R.color.white));
             getData(1);
             list_adapter.notifyDataSetChanged();
-            for(int i = 0;i<listView.getChildCount();i++){
-            	TextView textView= (TextView) listView.getChildAt(i).findViewById(R.id.item_name);
-            	textView.setTextColor(getResources().getColor(R.color.company01));
-            }
             break;   
         default:  
             break;  
--- P2P_Basket_Android/src/com/crowley/p2pnote/WaterFragment.java
+++ P2P_Basket_Android/src/com/crowley/p2pnote/WaterFragment.java
@@ -1,28 +1,71 @@
 package com.crowley.p2pnote;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.crowley.p2pnote.functions.ReturnList;
 import com.crowley.p2pnote.ui.listAdapter;
 
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class WaterFragment extends Fragment{
-	
-	private View view;
-	
+public class WaterFragment extends Fragment implements OnClickListener{
+		
 	private ListView listView;
 	private listAdapter list_adapter;
 	private List<Map<String, Object>> dataList;
 	
-	List list = new ArrayList();
+	private ReturnList returnList;
+	
+	private boolean time_des=true;
+	private boolean money_des=true;
+	private boolean profit_des=true;
+	
+	private RelativeLayout time_tab;
+	private RelativeLayout money_tab;
+	private RelativeLayout profit_tab;
+	
+	private TextView time_tab_text;
+	private TextView money_tab_text;
+	private TextView profit_tab_text;
+	
+	private ImageView time_tab_icon;
+	private ImageView money_tab_icon;
+	private ImageView profit_tab_icon;
+	
+	
+	public static final int[] PLATFORM_NAMES = {
+		R.string.company_name01,
+		R.string.company_name02,
+		R.string.company_name03,
+		R.string.company_name04,
+		R.string.company_name05,
+		R.string.company_name06,
+		R.string.company_name07,
+		R.string.company_name08,
+		R.string.company_name09
+		};
+	
+	public static final int[] PLATFORM_ICONS = {
+		R.drawable.company_icon01,
+		R.drawable.company_icon02,
+		R.drawable.company_icon03,
+		R.drawable.company_icon04,
+		R.drawable.company_icon05,
+		R.drawable.company_icon06,
+		R.drawable.company_icon07,
+		R.drawable.company_icon08,
+		R.drawable.company_icon09
+		};
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,62 +73,106 @@ public class WaterFragment extends Fragment{
 		// TODO Auto-generated method stub
 		View view = inflater.inflate(R.layout.water_fragment, container, false);
 
+		returnList = new ReturnList();
 		dataList=new ArrayList<Map<String,Object>>();
         listView=(ListView) view.findViewById(R.id.water_list_view);
-        getData();
+        time_tab=(RelativeLayout) view.findViewById(R.id.invest_time);
+        money_tab=(RelativeLayout) view.findViewById(R.id.invest_money);
+        profit_tab=(RelativeLayout) view.findViewById(R.id.invest_profit);
+        time_tab_text=(TextView) view.findViewById(R.id.invest_time_text);
+        money_tab_text=(TextView) view.findViewById(R.id.invest_money_text);
+        profit_tab_text=(TextView) view.findViewById(R.id.invest_profit_text);
+        time_tab_icon=(ImageView) view.findViewById(R.id.invest_time_icon);
+        money_tab_icon=(ImageView) view.findViewById(R.id.invest_money_icon);
+        profit_tab_icon=(ImageView) view.findViewById(R.id.invest_profit_icon);
+        getData(0,time_des);
         list_adapter=new listAdapter(this.getActivity(), dataList, R.layout.index_listview_item, new String[]{"time","item_icon","item_name","item_money","item_profit"}, new int[]{R.id.time,R.id.item_icon,R.id.item_name,R.id.item_money,R.id.item_profit});
         
-        /*SimpleAdapter.ViewBinder binder = new SimpleAdapter.ViewBinder() {
-        	@Override
-        	public boolean setViewValue(View view, Object object, String value) {
-        		if (view.equals((TextView) view.findViewById(R.id.item_name))){
-        			TextView ricetta = (TextView) view.findViewById(R.id.item_name);
-        			Log.i("m_info",(String) ricetta.getText());
-        			if(((String) ricetta.getText())=="陆金所-富赢人生"){
-        				ricetta.setText("陆金所-富赢人生");
-        				ricetta.setTextColor(getResources().getColor(R.color.company02));
-        				return true;
-        			}else if(((String) ricetta.getText())=="人人贷-优选计划"){
-        				ricetta.setText("人人贷-优选计划");
-        				ricetta.setTextColor(getResources().getColor(R.color.company01));
-        				return true;
-        			}else{
-        				return false;
-        			}
-        		}else{
-        			return false;
-        		}
-        	}
-        };
-        	            
-        list_adapter.setViewBinder(binder); */       
+               
         listView.setAdapter(list_adapter);
-        
+        time_tab.setOnClickListener(this);
+        money_tab.setOnClickListener(this);
+        profit_tab.setOnClickListener(this);
 		return view;
 	}
 
-	private void getData() {
+	private void getData(int type,boolean des) {
 		// TODO Auto-generated method stub
 		dataList.clear();
-    	for(int i=0;i<20;i++){
-    		Map<String, Object> map=new HashMap<String, Object>();
-    		if(i%2==0){
-    			map.put("time", "2014-10-1"+i);
-        		map.put("item_icon", R.drawable.company_icon02);
-        		map.put("item_name", "陆金所-富赢人生");
-        		map.put("item_money", "12,000");
-        		map.put("item_profit", "12%");
-        		list.add("0");
-    		}else{    			
-        		map.put("time", "2014-08-1"+i);
-        		map.put("item_icon", R.drawable.company_icon01);
-        		map.put("item_name", "人人贷-优选计划");
-        		map.put("item_money", "11,000");
-        		map.put("item_profit", "8%");
-        		list.add("1");
-    		}    		
-    		dataList.add(map);    		
-    	}
+		List<Map<String, Object>> temp=new ArrayList<Map<String,Object>>();
+		temp=returnList.waterSort(this.getActivity(),type,des);
+		for(int i=0;i<temp.size();i++){
+			dataList.add(temp.get(i));
+		}
+	}
+	
+	public void tabReset(){
+		time_tab_text.setTextColor(getResources().getColor(R.color.light_black));
+		money_tab_text.setTextColor(getResources().getColor(R.color.light_black));
+		profit_tab_text.setTextColor(getResources().getColor(R.color.light_black));
+		if(time_des){
+			time_tab_icon.setImageResource(R.drawable.water_arrow_down);
+		}else{
+			time_tab_icon.setImageResource(R.drawable.water_arrow_up);
+		}
+		if(money_des){
+			money_tab_icon.setImageResource(R.drawable.water_arrow_down);
+		}else{
+			money_tab_icon.setImageResource(R.drawable.water_arrow_up);
+		}
+		if(profit_des){
+			profit_tab_icon.setImageResource(R.drawable.water_arrow_down);
+		}else{
+			profit_tab_icon.setImageResource(R.drawable.water_arrow_up);
+		}		
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.invest_time:{
+			time_des=!time_des;
+			tabReset();
+			time_tab_text.setTextColor(getResources().getColor(R.color.tab_text_chosen));
+			if(time_des){
+				time_tab_icon.setImageResource(R.drawable.water_arrow_down_chosen);
+			}else{
+				time_tab_icon.setImageResource(R.drawable.water_arrow_up_chosen);
+			}
+			getData(0,time_des);
+			list_adapter.notifyDataSetChanged();
+			break;			
+		}
+		case R.id.invest_money:{
+			money_des=!money_des;
+			tabReset();
+			money_tab_text.setTextColor(getResources().getColor(R.color.tab_text_chosen));
+			if(money_des){
+				money_tab_icon.setImageResource(R.drawable.water_arrow_down_chosen);
+			}else{
+				money_tab_icon.setImageResource(R.drawable.water_arrow_up_chosen);
+			}
+			getData(1,money_des);
+			list_adapter.notifyDataSetChanged();
+			break;			
+		}
+		case R.id.invest_profit:{
+			profit_des=!profit_des;
+			tabReset();
+			profit_tab_text.setTextColor(getResources().getColor(R.color.tab_text_chosen));
+			if(profit_des){
+				profit_tab_icon.setImageResource(R.drawable.water_arrow_down_chosen);
+			}else{
+				profit_tab_icon.setImageResource(R.drawable.water_arrow_up_chosen);
+			}
+			getData(2,profit_des);
+			list_adapter.notifyDataSetChanged();
+			break;			
+		}
+		default:
+			break;
+		}
 	}
 
 }
